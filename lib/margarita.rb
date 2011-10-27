ff20e0a3da1feed50619fa65a9ed71236e981843
cookiebatch <keymint1498@gmail.com>
2011-10-27 08:04:02 -0400

    added "/n"'s, added the link text to put in the XML file

--- lib/margarita.rb
+++ lib/margarita.rb
@@ -8,8 +8,20 @@ puts 'What should the background color be?'
 	background = gets.chomp
 
 
+puts 'add this to your style.css:'
+
 puts "TITLE { \n" + "color:" + color + "\n" + "}"
 puts "DESCRIPTION { \n" + "color:" + color + "\n" + "}"
 puts "GUID { \n" + "color:" + color + "\n" +  "}"
 puts "PUBDATE { \n" + "color:" + color + "\n" + "}"
 puts "RSS { \n" + "background-color:" + color + "\n" + "}"
+
+puts "    "
+puts "    "
+
+
+puts "add the folllowing to your rss.xml:"
+
+puts '<?xml-stylesheet type="text/css" href="style.css"?>'
+
+puts 'note that if your CSS file is not "syle.css", fill in the name where it says that.'
--- lib/margaritaatom.rb
+++ lib/margaritaatom.rb
@@ -7,6 +7,8 @@ puts 'What should the background color be?'
 
 	background = gets.chomp
 
+puts "add this to yur style.css:"
+
 
 puts "TITLE { \n" + "color:" + color + "\n" + "}"
 puts "SUBTITLE { \n" + "color:" + color + "\n" + "}"
@@ -14,3 +16,11 @@ puts "GUID { \n" + "color:" + color + "\n" +  "}"
 puts "PUBDATE { \n" + "color:" + color + "\n" + "}"
 puts "FEED { \n" + "background-color:" + color + "\n" + "}"
 puts "SUMMARY { \n" + "color:" + color + "\n" + "}"
+
+puts "    "
+puts "    "
+puts "add the folllowing to your atom.xml:"
+
+puts '<?xml-stylesheet type="text/css" href="style.css"?>'
+
+puts 'note that if your CSS file is not "syle.css", fill in the name where it says that.'
